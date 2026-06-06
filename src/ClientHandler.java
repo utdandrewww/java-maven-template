c6330d6c63da26652d18d3a6154f303fa7482664
utdandrewww <a.s.j.watts@gmail.com>
2026-06-06 21:46:44 +0100

    Server + Client Handler

---
+++ src/ClientHandler.java
@@ -0,0 +1,10 @@
+public class ClientHandler implements Runnable {
+    
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+
+    @Override
+    public void run() {
+        // Handle client communication here
+    }
+}
